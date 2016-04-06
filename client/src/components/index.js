9f7d3ce712bc4adaca6caa65e23824a285c224ac
Jessica Tran Hur <jesshur824@gmail.com>
2016-04-06 10:36:25 -0500

    reconfigure webpack config, add jshintrc file to rid of unwanted es6 warnings from jshint

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,9 @@
 import appDr from './app/app.js';
+import header from './header/header.js';
 
 export default function(angularModule) {
 
   appDr(angularModule);
+  header(angularModule);
 
 }
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,31 +3,60 @@ const DefinePlugin = require('webpack').DefinePlugin;
 const path = require( 'path' );
 const entry = './src/entry.js';
 
-module.exports = {
+const webpack = require('webpack');
+const merge = require('webpack-merge'); //Concatenates arrays and merges objs. Used to merge "common" config obj on line 10 with custom configs for each npm script
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  build: path.join(__dirname, '../server/public'),
+  images: path.join(__dirname, 'src/assets')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  // Entry accepts a path or an object of entries. We'll be using the
+  // latter form given it's convenient with more complex configurations.
   entry,
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      }),
+      new DefinePlugin({
+        BASE_URL: JSON.stringify(process.env.BASE_URL_DEV || '')
+      })
+  ],
   output: {
-    path: path.resolve( __dirname, '../server/public' ),
+    path: PATHS.build,
     filename: 'bundle.js'
   },
   devtool: 'source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new DefinePlugin({
-      BASE_URL: JSON.stringify(process.env.BASE_URL || '')
-      // CLIENT_ID: JSON.stringify(process.env.CLIENT_ID || '')
-    })
-  ],
   module: {
-    preLoaders: [
+    preLoaders: [ //preLoaders section of the configuration gets executed before loaders
       {
-          test: /\.js$/, // include .js files
-          exclude: /node_modules/, // exclude any and all files in the node_modules folder
-          loader: "jshint-loader"
+        test: /\.js?$/,
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        loader: "jshint-loader"
       }
     ],
     loaders: [
+      {
+        // Test expects a RegExp! Note the slashes!
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file',
+        include: PATHS.images
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -38,14 +67,47 @@ module.exports = {
             // plugins: ['transform-runtime']
           }
       },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      },
-      {
-        test: /\.html$/,
-        loader: 'html'
-      }
     ]
   }
 };
+
+// Default configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
+
